dfe62800a4c687d2b7c805a5c4a64adad562f81d
miguelarteaga <marteagavalle@gmail.com>
2018-12-19 20:24:13 +0100

    Node

--- Node/ex4.js
+++ Node/ex4.js
@@ -1,12 +1,26 @@
-var fs=require('fs')
-var dir = process.argv[2]
-var extension= "." + process.argv[3]
 
-console.log(dir)
-console.log(extension)
+//var fs=require('fs')
+//var dir = process.argv[2]
+//var extension= "." + process.argv[3]
 
-var asdf = fs.readdir(dir)
+//console.log(dir)
+//console.log(extension)
 
-fs.readdir(dir, function callback (err,list){
-        console.log(list)
+//var asdf = fs.readdir(dir)
+
+//fs.readdir(dir, function callback (err,list){
+//        console.log(list)
+//})
+
+
+const fs = require('fs')
+const file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+  if (err) {
+    return console.log(err)
+  }
+  // fs.readFile(file, 'utf8', callback) también se puede utilizar
+  const lines = contents.toString().split('\n').length - 1
+  console.log(lines)
 })
